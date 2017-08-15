f40deef4860644e3b72a0ecc60ef2a25e1b8771c
mimbres <1ronyar@gmail.com>
2017-08-16 00:15:32 +0900

    updating preprocess_data.py for YH dataloader

--- preprocess_data.py
+++ preprocess_data.py
@@ -9,11 +9,18 @@ Created on Tue Aug 15 18:39:48 2017
 
 import numpy as np
 import h5py
+from sklearn.preprocessing import StandardScaler
 
 
 
 
 
+# def preprocess_YH_data():
+# IN:
+# OUT: 
+# X_train, X_test, y_train, y_test, tdim, dim
+#
+# dim is feature dimension, tdim is time dimension  
 def preprocess_YH_data():
     
     #%% Read files
@@ -44,11 +51,38 @@ def preprocess_YH_data():
     X_test[0:idxSim_val.shape[0],1,:,:] =  X[idxSim_val[:,1],:,:]
     X_test[idxSim_val.shape[0]:,0,:,:] =  X[idxDis_val[:,0],:,:]
     X_test[idxSim_val.shape[0]:,1,:,:] =  X[idxDis_val[:,1],:,:]
+    
+    
+    #%% create labels
+    y_train = np.concatenate((np.ones((idxSim_tr.shape[0],1)),np.zeros((idxDis_tr.shape[0],1))),axis=0)
+    y_test = np.concatenate((np.ones((idxSim_val.shape[0],1)),np.zeros((idxDis_val.shape[0],1))), axis=0)
+
+    # apply OTI for all data
+    print('apply OTI...')
+    for i in range(0,X_train.shape[0]):
+        cover1, cover2 = oti(X_train[i,0,:,:],X_train[i,1,:,:],dim)
+        X_train[i,0,:,:], X_train[i,1,:,:] = cover1, cover2
+
+    for i in range(0,X_test.shape[0]):
+        cover1, cover2 = oti(X_test[i,0,:,:],X_test[i,1,:,:],dim)
+        X_test[i,0,:,:], X_test[i,1,:,:] = cover1, cover2
+        
+        
+    # standardize
+    print('standardize features...')
+    scaler = StandardScaler(copy=False).fit(X_train.reshape(X_train.shape[0]*2*tdim,dim))
+    X_train = scaler.transform(X_train.reshape(X_train.shape[0]*2*tdim,dim)).reshape(X_train.shape[0],2,tdim,dim)
+    X_test = scaler.transform(X_test.reshape(X_test.shape[0]*2*tdim,dim)).reshape(X_test.shape[0],2,tdim,dim)    
+        
+
+    
     #%%
-    
-    return
-    
-    
+
+
+    return X_train, X_test, y_train, y_test, tdim, dim
+
+
+
 
 
 # Optimal transposition index (OTI) implementation: returns transposed chromas
@@ -66,4 +100,3 @@ def oti(cover1,cover2,chroma_dim):
     cover2_shifted = np.roll(cover2, oti, axis=1) # to return shifted chroma
     
     return cover1, cover2_shifted
-
