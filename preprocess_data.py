efd6686321fd4667707c6e298e986021de270e69
mimbres <1ronyar@gmail.com>
2017-08-16 00:25:29 +0900

    add comments on preprocess_data.py

--- preprocess_data.py
+++ preprocess_data.py
@@ -13,16 +13,12 @@ from sklearn.preprocessing import StandardScaler
 
 
 
-
-
-# def preprocess_YH_data():
+# def preprocess_YH_data()
 # IN:
-# OUT: 
-# X_train, X_test, y_train, y_test, tdim, dim
-#
-# dim is feature dimension, tdim is time dimension  
+# OUT: X_train, X_test, y_train, y_test, tdim, dim
+# Goal: pack train/test features with labels from .h5 data.
+#       dim is feature dimension, tdim is time dimension  
 def preprocess_YH_data():
-    
     #%% Read files
     print('read files...')
     with h5py.File('./data/training/CP_1000ms_training_s2113_d2113_170106223452.h5','r') as f:
@@ -32,12 +28,9 @@ def preprocess_YH_data():
         idxSim_tr = np.asarray(f['idxSim_train']).astype(int)-1
         idxSim_val = np.asarray(f['idxSim_validate']).astype(int)-1
 
-
-
     #%% dim is feature dimension, tdim is time dimension
     tdim = X.shape[1]
     dim = X.shape[2] 
-
 
     #%% read indices and put data in X_train and X_test
     X_train = np.zeros((idxSim_tr.shape[0]+idxDis_tr.shape[0],2,tdim,dim))  
@@ -52,7 +45,6 @@ def preprocess_YH_data():
     X_test[idxSim_val.shape[0]:,0,:,:] =  X[idxDis_val[:,0],:,:]
     X_test[idxSim_val.shape[0]:,1,:,:] =  X[idxDis_val[:,1],:,:]
     
-    
     #%% create labels
     y_train = np.concatenate((np.ones((idxSim_tr.shape[0],1)),np.zeros((idxDis_tr.shape[0],1))),axis=0)
     y_test = np.concatenate((np.ones((idxSim_val.shape[0],1)),np.zeros((idxDis_val.shape[0],1))), axis=0)
@@ -66,26 +58,24 @@ def preprocess_YH_data():
     for i in range(0,X_test.shape[0]):
         cover1, cover2 = oti(X_test[i,0,:,:],X_test[i,1,:,:],dim)
         X_test[i,0,:,:], X_test[i,1,:,:] = cover1, cover2
-        
-        
+               
     # standardize
     print('standardize features...')
     scaler = StandardScaler(copy=False).fit(X_train.reshape(X_train.shape[0]*2*tdim,dim))
     X_train = scaler.transform(X_train.reshape(X_train.shape[0]*2*tdim,dim)).reshape(X_train.shape[0],2,tdim,dim)
     X_test = scaler.transform(X_test.reshape(X_test.shape[0]*2*tdim,dim)).reshape(X_test.shape[0],2,tdim,dim)    
         
-
-    
     #%%
-
-
     return X_train, X_test, y_train, y_test, tdim, dim
 
 
 
 
-
-# Optimal transposition index (OTI) implementation: returns transposed chromas
+# def oti(cover1,cover2,chroma_dim):
+# IN: cover1, cover2, chroma_dim
+# OUT: cover1, cover2_shifted
+# Goal: Optimal transposition index (OTI) implementation, returns transposed chromas
+#
 def oti(cover1,cover2,chroma_dim): 
     cover1_mean = np.sum(cover1,axis=0)/np.max(np.sum(cover1,axis=0)) # get global HPCP first
     cover2_mean = np.sum(cover2,axis=0)/np.max(np.sum(cover2,axis=0))
